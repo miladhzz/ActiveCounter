358c770bb4d306e3524f1ca2a51ac7f405f1aac0
miladhzz <miladhzz@gmail.com>
2026-07-02 01:20:07 +0330

    add new icon

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -73,9 +73,29 @@ namespace ActiveCounter.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
+        internal static System.Drawing.Icon pink {
+            get {
+                object obj = ResourceManager.GetObject("pink", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
         internal static System.Drawing.Icon red {
             get {
                 object obj = ResourceManager.GetObject("red", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon red2 {
+            get {
+                object obj = ResourceManager.GetObject("red2", resourceCulture);
                 return ((System.Drawing.Icon)(obj));
             }
         }
