736913e27e4c66626810ca3c7041cd65a9fe7493
miladhzz <miladhzz@gmail.com>
2026-07-02 01:15:51 +0330

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace ActiveCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            btnStart = new Button();
+            btnStop = new Button();
+            lblHours = new Label();
+            lblMinutes = new Label();
+            lblSeconds = new Label();
+            buttonReset = new Button();
+            textBox1 = new TextBox();
+            checkBox1 = new CheckBox();
+            buttonUp = new Button();
+            buttonDown = new Button();
+            textBoxMin = new TextBox();
+            groupBoxUpDown = new GroupBox();
+            groupBoxUpDown.SuspendLayout();
+            SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(88, 138);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(112, 34);
+            btnStart.TabIndex = 0;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // btnStop
+            // 
+            btnStop.Location = new Point(340, 138);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(112, 34);
+            btnStop.TabIndex = 1;
+            btnStop.Text = "Stop";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
+            // 
+            // lblHours
+            // 
+            lblHours.AutoSize = true;
+            lblHours.Font = new Font("Segoe UI", 12F);
+            lblHours.Location = new Point(144, 63);
+            lblHours.Name = "lblHours";
+            lblHours.Size = new Size(78, 32);
+            lblHours.TabIndex = 2;
+            lblHours.Text = "label1";
+            // 
+            // lblMinutes
+            // 
+            lblMinutes.AutoSize = true;
+            lblMinutes.Font = new Font("Segoe UI", 12F);
+            lblMinutes.Location = new Point(232, 63);
+            lblMinutes.Name = "lblMinutes";
+            lblMinutes.Size = new Size(78, 32);
+            lblMinutes.TabIndex = 3;
+            lblMinutes.Text = "label1";
+            // 
+            // lblSeconds
+            // 
+            lblSeconds.AutoSize = true;
+            lblSeconds.Font = new Font("Segoe UI", 12F);
+            lblSeconds.Location = new Point(320, 63);
+            lblSeconds.Name = "lblSeconds";
+            lblSeconds.Size = new Size(78, 32);
+            lblSeconds.TabIndex = 4;
+            lblSeconds.Text = "label1";
+            // 
+            // buttonReset
+            // 
+            buttonReset.Location = new Point(207, 202);
+            buttonReset.Name = "buttonReset";
+            buttonReset.Size = new Size(112, 34);
+            buttonReset.TabIndex = 5;
+            buttonReset.Text = "reset";
+            buttonReset.UseVisualStyleBackColor = true;
+            buttonReset.Click += buttonReset_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 15F);
+            textBox1.Location = new Point(99, 254);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(364, 47);
+            textBox1.TabIndex = 6;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(198, 12);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(171, 29);
+            checkBox1.TabIndex = 7;
+            checkBox1.Text = "فعالسازی فوکوس";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // buttonUp
+            // 
+            buttonUp.Font = new Font("Segoe UI", 12F);
+            buttonUp.Location = new Point(47, 23);
+            buttonUp.Name = "buttonUp";
+            buttonUp.Size = new Size(74, 56);
+            buttonUp.TabIndex = 8;
+            buttonUp.Text = "+";
+            buttonUp.UseVisualStyleBackColor = true;
+            buttonUp.Click += buttonUp_Click;
+            // 
+            // buttonDown
+            // 
+            buttonDown.Font = new Font("Segoe UI", 12F);
+            buttonDown.Location = new Point(47, 152);
+            buttonDown.Name = "buttonDown";
+            buttonDown.Size = new Size(74, 56);
+            buttonDown.TabIndex = 9;
+            buttonDown.Text = "-";
+            buttonDown.UseVisualStyleBackColor = true;
+            buttonDown.Click += buttonDown_Click;
+            // 
+            // textBoxMin
+            // 
+            textBoxMin.Font = new Font("Segoe UI", 15F);
+            textBoxMin.Location = new Point(32, 88);
+            textBoxMin.Name = "textBoxMin";
+            textBoxMin.Size = new Size(109, 47);
+            textBoxMin.TabIndex = 10;
+            textBoxMin.Text = "10";
+            // 
+            // groupBoxUpDown
+            // 
+            groupBoxUpDown.Controls.Add(textBoxMin);
+            groupBoxUpDown.Controls.Add(buttonUp);
+            groupBoxUpDown.Controls.Add(buttonDown);
+            groupBoxUpDown.Location = new Point(694, 49);
+            groupBoxUpDown.Name = "groupBoxUpDown";
+            groupBoxUpDown.Size = new Size(171, 229);
+            groupBoxUpDown.TabIndex = 11;
+            groupBoxUpDown.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(877, 325);
+            Controls.Add(groupBoxUpDown);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox1);
+            Controls.Add(buttonReset);
+            Controls.Add(lblSeconds);
+            Controls.Add(lblMinutes);
+            Controls.Add(lblHours);
+            Controls.Add(btnStop);
+            Controls.Add(btnStart);
+            Name = "Form1";
+            Text = "Form1";
+            Activated += MainForm_Activated;
+            Deactivate += MainForm_Deactivate;
+            Resize += MainForm_Resize;
+            groupBoxUpDown.ResumeLayout(false);
+            groupBoxUpDown.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private Button btnStart;
+        private Button btnStop;
+        private Label lblHours;
+        private Label lblMinutes;
+        private Label lblSeconds;
+        private Button buttonReset;
+        private TextBox textBox1;
+        private CheckBox checkBox1;
+        private Button buttonUp;
+        private Button buttonDown;
+        private TextBox textBoxMin;
+        private GroupBox groupBoxUpDown;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ActiveCounter
+{
+    public partial class Form1 : Form
+    {
+        private int hours;
+        private int minutes;
+        private int seconds;
+
+        private bool isRunning;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitializeTimer();
+            UpdateLabels();
+            UpdateUI();
+        }
+
+        private void InitializeTimer()
+        {
+            timer1.Interval = 1000;
+            timer1.Enabled = false;
+        }
+
+        private void StartTimer()
+        {
+            isRunning = true;
+            timer1.Start();
+            UpdateUI();
+        }
+
+        private void StopTimer()
+        {
+            isRunning = false;
+            timer1.Stop();
+            UpdateUI();
+        }
+
+        private void ResetTimer()
+        {
+            StopTimer();
+
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+
+            UpdateLabels();
+        }
+
+        private void UpdateUI()
+        {
+            btnStart.Enabled = !isRunning;
+            btnStop.Enabled = isRunning;
+
+            buttonReset.Enabled = !isRunning;
+            textBox1.Enabled = !isRunning;
+            groupBoxUpDown.Enabled = !isRunning;
+
+            BackColor = isRunning ? Color.Green : Color.Red;
+            Icon = isRunning
+                ? Properties.Resources.green
+                : Properties.Resources.red;
+        }
+
+        private void UpdateLabels()
+        {
+            lblHours.Text = $"{hours:D2}";
+            lblMinutes.Text = $"{minutes:D2}";
+            lblSeconds.Text = $"{seconds:D2}";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartTimer();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            StopTimer();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            ResetTimer();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+
+            if (seconds >= 60)
+            {
+                seconds = 0;
+                minutes++;
+            }
+
+            if (minutes >= 60)
+            {
+                minutes = 0;
+                hours++;
+            }
+
+            UpdateLabels();
+        }
+
+        private void MainForm_Deactivate(object sender, EventArgs e)
+        {
+            if (!checkBox1.Checked)
+                return;
+
+            if (isRunning)
+                StopTimer();
+        }
+
+        private void MainForm_Activated(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked && !isRunning)
+                StartTimer();
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized &&
+                checkBox1.Checked &&
+                isRunning)
+            {
+                StopTimer();
+            }
+        }
+
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBoxMin.Text, out int value))
+            {
+                minutes += value;
+                NormalizeTime();
+                UpdateLabels();
+            }
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBoxMin.Text, out int value))
+            {
+                minutes -= value;
+
+                if (minutes < 0)
+                    minutes = 0;
+
+                NormalizeTime();
+                UpdateLabels();
+            }
+        }
+
+        private void NormalizeTime()
+        {
+            while (minutes >= 60)
+            {
+                minutes -= 60;
+                hours++;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
